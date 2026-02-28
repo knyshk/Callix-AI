bf6c927a996946a1a6eee58b700dcc06c143db1d
Kanishk_Jain_0510 <kanishk.jain0510@gmail.com>
2026-02-28 21:09:56 +0530

    api

--- voice_agent/main.py
+++ voice_agent/main.py
@@ -1,18 +1,30 @@
+# # main.py
+
+# from fastapi import FastAPI
+# from pydantic import BaseModel
+# from app.core.pipeline import run_pipeline
+
+
+# app = FastAPI()
+
+
+# class ConversationRequest(BaseModel):
+#     text: str
+
+
+# @app.post("/conversation")
+# async def conversation(request: ConversationRequest):
+#     result = run_pipeline(request.text)
+#     return result
+
+
+
+
+
+
 # main.py
 
-from fastapi import FastAPI
-from pydantic import BaseModel
-from app.core.pipeline import run_pipeline
+import uvicorn
 
-
-app = FastAPI()
-
-
-class ConversationRequest(BaseModel):
-    text: str
-
-
-@app.post("/conversation")
-async def conversation(request: ConversationRequest):
-    result = run_pipeline(request.text)
-    return result
+if __name__ == "__main__":
+    uvicorn.run("app.api.server:app", host="0.0.0.0", port=8000, reload=True)
